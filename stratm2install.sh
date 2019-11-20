09dc34c3256a7ff7dbf926b6abf75a85449cbd37
jwisecarver-mm <50086133+jwisecarver-mm@users.noreply.github.com>
2019-11-19 16:14:43 -0800

    use printf so newlines work, rid of double whitespaces

--- stratm2install.sh
+++ stratm2install.sh
@@ -2,21 +2,21 @@
 # M2 installer for Stratus (Includes MageMojo Cron, Varnish, and Redis)
 # A Mark Muyskens production.... with some base stuff stolen from Jwise.... and ok, I stole the Stratus CLI grep/awk stuff from Jackie...
 
-echo "Let's get to installing M2.... You best have Redis enabled or you'll be crying when this is over.... "
+printf "Let's get to installing M2.... You best have Redis enabled or you'll be crying when this is over.... "
 
 #PHP 5 needs to die.
 _php5=$(php -v|grep --only-matching --perl-regexp "5\.\\d+\.\\d+" -m1);
 if [ -z "$_php5" ]
 then
-        echo "\nDetecting PHP - OK\n" 
+        printf "\nDetecting PHP - OK\n"
         php -v
-	echo "\n"
+	printf "\n"
 else
-        echo  "You're running PHP 5 - FIX IT NOW!"
+        printf "You're running PHP 5 - FIX IT NOW!"
         exit
 fi
 
-echo  "What's the URL?"
+printf "What's the URL?"
 read _url
 
 # Noobproof URL fail safe
@@ -24,7 +24,7 @@ case "$_url" in
 https://*)
 ;;
 *)
-echo "Let's try that again... let's not forget https:// this time."
+printf "Let's try that again... let's not forget https:// this time."
 read _url
 esac
 
@@ -32,30 +32,30 @@ case "$_url" in
 */)
     ;;
 *)
-    echo "Noob mistake - you forgot the trailing slash. I'll add it for you. You've caught me in a good mood. You should probably escape now if the following doesn't look valid:"
+    printf "Noob mistake - you forgot the trailing slash. I'll add it for you. You've caught me in a good mood. You should probably escape now if the following doesn't look valid:"
     _url="${_url}/"
-    echo $_url
+    printf $_url
     ;;
 esac
 # End Noobproof
 
-echo  "How about a firstname?"
+printf "How about a firstname?"
 read _firstname
 
 # Quick shoutout to anyone named Mark
 case "$_firstname" in
 Mark)
-echo "Such a great name...."
+printf "Such a great name...."
 ;;
 *)
 esac
 
 # Back to work now...
-echo "You probably should provide a lastname now..."
+printf "You probably should provide a lastname now..."
 read _lastname
-echo "Groovy - now I just need an admin email."
+printf "Groovy - now I just need an admin email."
 read _adminemail
-echo "Thanks, let me do some stuff now."
+printf "Thanks, let me do some stuff now."
 
 # Random joke time...
 printf "\nEnjoy a random dad joke while you wait...\n"
@@ -90,7 +90,7 @@ redis-cli -h redis -p 6379 FLUSHALL
 redis-cli -h redis-config-cache -p 6381 FLUSHALL
 redis-cli -h redis-session -p 6380 FLUSHALL
 
-echo "Enabling Redis"
+printf "Enabling Redis"
 php bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=redis-config-cache --cache-backend-redis-db=0 --cache-backend-redis-port=6381
 php bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=0 --page-cache-redis-port=6379
 php bin/magento setup:config:set --session-save=redis --session-save-redis-host=redis-session --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6380
@@ -100,12 +100,12 @@ php bin/magento setup:config:set --session-save=redis --session-save-redis-host=
 _varnishcheck=$(curl -X 'PURGE' -H'X-Magento-Tags-Pattern: .*' varnish 2>/dev/null);
 case "$_varnishcheck" in
 *Varnish*)
-echo "\nVarnish has been detected and will be configured."
+printf "\nVarnish has been detected and will be configured."
 php bin/magento config:set --scope=default --scope-code=0 system/full_page_cache/caching_application 2
 php bin/magento config:set --scope=default --scope-code=0 system/full_page_cache/varnish/backend_host nginx
 php bin/magento setup:config:set --http-cache-hosts=varnish
 ;;
-*) echo "\nVarnish was NOT detected and will be SKIPPED.\n"
+*) printf "\nVarnish was NOT detected and will be SKIPPED.\n"
 esac
 
 # Fail Safe cache clear and autoscaling reinit (in cases of reinstall)
