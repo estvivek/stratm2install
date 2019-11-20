6e56bbfcce23007a960aea50fae1a49a46cdd92b
jwisecarver-mm <50086133+jwisecarver-mm@users.noreply.github.com>
2019-11-19 16:53:20 -0800

    Fix line endings lul, more formatting because OCD

--- stratm2install.sh
+++ stratm2install.sh
@@ -2,63 +2,69 @@
 # M2 installer for Stratus (Includes MageMojo Cron, Varnish, and Redis)
 # A Mark Muyskens production.... with some base stuff stolen from Jwise.... and ok, I stole the Stratus CLI grep/awk stuff from Jackie...
 
-printf "Let's get to installing M2.... You best have Redis enabled or you'll be crying when this is over.... "
+printf "Let's get to installing M2.... You best have Redis enabled or you'll be crying when this is over....\n\n"
 
 #PHP 5 needs to die.
 _php5=$(php -v|grep --only-matching --perl-regexp "5\.\\d+\.\\d+" -m1);
 if [ -z "$_php5" ]
 then
-        printf "\nDetecting PHP - OK\n"
-        php -v
-	printf "\n"
+    printf "Detecting PHP - OK\n"
+    php -v
+	printf "\n\n"
 else
-        printf "You're running PHP 5 - FIX IT NOW!"
-        exit
+    printf "You're running PHP 5 - FIX IT NOW!\n"
+    exit
 fi
 
-printf "What's the URL?"
+printf "What's the URL?\n"
 read _url
+printf "\n"
 
 # Noobproof URL fail safe
 case "$_url" in
 https://*)
 ;;
 *)
-printf "Let's try that again... let's not forget https:// this time."
+printf "Let's try that again... let's not forget https:// this time.\n"
 read _url
+printf "\n"
 esac
 
 case "$_url" in
 */)
     ;;
 *)
-    printf "Noob mistake - you forgot the trailing slash. I'll add it for you. You've caught me in a good mood. You should probably escape now if the following doesn't look valid:"
+    printf "Noob mistake - you forgot the trailing slash. I'll add it for you. You've caught me in a good mood. You should probably escape now if the following doesn't look valid:\n"
     _url="${_url}/"
     printf $_url
+    printf "\n\n"
     ;;
 esac
 # End Noobproof
 
-printf "How about a firstname?"
+printf "How about a firstname?\n"
 read _firstname
+printf "\n"
 
 # Quick shoutout to anyone named Mark
 case "$_firstname" in
 Mark)
-printf "Such a great name...."
+printf "Such a great name....\n\n"
 ;;
 *)
 esac
 
 # Back to work now...
-printf "You probably should provide a lastname now..."
+printf "You probably should provide a lastname now...\n"
 read _lastname
-printf "Groovy - now I just need an admin email."
+printf "\n"
+printf "Groovy - now I just need an admin email.\n"
 read _adminemail
-printf "Thanks, let me do some stuff now."
+printf "\n"
+printf "Thanks, let me do some stuff now.\n\n"
 
 # Random joke time...
-printf "\nEnjoy a random dad joke while you wait...\n"
+printf "Enjoy a random dad joke while you wait:\n"
 curl -H "Accept: text/plain" https://icanhazdadjoke.com/
 printf "\n\n"
 
@@ -90,22 +96,21 @@ redis-cli -h redis -p 6379 FLUSHALL
 redis-cli -h redis-config-cache -p 6381 FLUSHALL
 redis-cli -h redis-session -p 6380 FLUSHALL
 
-printf "Enabling Redis"
+printf "Enabling Redis..\n"
 php bin/magento setup:config:set --cache-backend=redis --cache-backend-redis-server=redis-config-cache --cache-backend-redis-db=0 --cache-backend-redis-port=6381
 php bin/magento setup:config:set --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=0 --page-cache-redis-port=6379
-php bin/magento setup:config:set --session-save=redis --session-save-redis-host=redis-session --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6380
-
+printf "y" | php bin/magento setup:config:set --session-save=redis --session-save-redis-host=redis-session --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6380
 
 #Checking for Varnish
 _varnishcheck=$(curl -X 'PURGE' -H'X-Magento-Tags-Pattern: .*' varnish 2>/dev/null);
 case "$_varnishcheck" in
 *Varnish*)
-printf "\nVarnish has been detected and will be configured."
+printf "Varnish has been detected and will be configured..\n"
 php bin/magento config:set --scope=default --scope-code=0 system/full_page_cache/caching_application 2
 php bin/magento config:set --scope=default --scope-code=0 system/full_page_cache/varnish/backend_host nginx
 php bin/magento setup:config:set --http-cache-hosts=varnish
 ;;
-*) printf "\nVarnish was NOT detected and will be SKIPPED.\n"
+*) printf "Varnish was NOT detected and will be SKIPPED.\n\n"
 esac
 
 # Fail Safe cache clear and autoscaling reinit (in cases of reinstall)
